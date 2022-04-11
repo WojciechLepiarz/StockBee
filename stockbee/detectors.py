15578c8845a2f4142cf45dad07da58a00e6f81c1
Wojciech Lepiarz <wojciech.lepiarz@gmail.com>
2022-04-11 18:51:27 +0200

    Added few lines of code to pinbar detector.

--- stockbee/detectors.py
+++ stockbee/detectors.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 
-def pinbar_detector(data: pd.DataFrame):
-    data["pinbar"] = []
+def pinbar_detector(data: pd.DataFrame, labelPinBuy: str="PIN_Buy", labelPinSell: str="PIN_Sell"):
+    data[labelPinBuy] = np.ones((len(data.index), 1), dtype=np.int)
+    data[labelPinSell] = np.zeros((len(data.index), 1), dtype=np.int)
     return data
--- tests/test_pinbar_detector.py
+++ tests/test_pinbar_detector.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
-"""Tests for `stockbee` package."""
+"""Tests for `stockbee.detectors.pinbar_detector`."""
 
-import pytest
 import pandas as pd
+import pytest
+
 from stockbee.detectors import pinbar_detector
 
-@pytest.fixture
-def dataframe_with_data():
-    new_dataframe = pd.DataFrame()
+defaultPinBuyLabel = "PIN_Buy"
+defaultPinSellLabel = "PIN_Sell"
+
+# @pytest.fixture
+# def dataframe_with_data():
+#     new_dataframe = pd.DataFrame()
     
 def test_if_pinbar_detector_inputs_and_outputs_dataframe():
     dataframe = pd.DataFrame([])
@@ -16,6 +20,19 @@ def test_if_pinbar_detector_inputs_and_outputs_dataframe():
 
 def test_if_pinbar_detector_returns_dataframe_with_column_pinbar_added():
     dataframe_out = pinbar_detector(pd.DataFrame([]))
-    assert "pinbar" in dataframe_out.columns
+    assert defaultPinBuyLabel in dataframe_out.columns
+    assert defaultPinSellLabel in dataframe_out.columns
+
+def test_custom_pinbar_labels():
+    dataframe_out = pinbar_detector(pd.DataFrame([]), labelPinBuy="BuyPin", labelPinSell="SellPin")
+    assert "BuyPin" in dataframe_out.columns
+    assert "SellPin" in dataframe_out.columns
 
 def test_if_pinbar_column_is_0s_and_1s():
+    inputDataFrame = pd.read_csv(".\\tests\\test_data\\test_stock_data.mst", index_col=1)
+    inputDataFrame.index = pd.to_datetime(inputDataFrame.index, format="%Y%m%d")
+    inputDataFrame = pinbar_detector(inputDataFrame)
+    inputDataFrame.loc[inputDataFrame[defaultPinBuyLabel] == 1] = 0
+    inputDataFrame.loc[inputDataFrame[defaultPinSellLabel] == 1] = 0
+    assert inputDataFrame[defaultPinBuyLabel].sum() == 0
+    assert inputDataFrame[defaultPinSellLabel].sum() == 0
