ec4a2d88cacfa67ebc334d84b8253011b3132a65
WojciechLepiarz <wojciech.lepiarz@gmail.com>
2022-08-05 16:09:17 +0200

    Started work on strategy simulator

---
+++ stockbee/strategy_simulator.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+class Strategy:
+    def __init__(self):
+        return
+
+    def simulate(self, data: pd.DataFrame, budget: float = 10000., 
+                 leverage: int = 1) -> pd.DataFrame: 
+        return pd.DataFrame([])
